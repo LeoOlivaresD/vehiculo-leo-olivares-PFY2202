ae1cad8442bef7aacd85aa607c08ef8e1875219b
LeoOlivaresD <cspraword@gmail.com>
2025-11-04 00:52:57 -0300

    Se implementa tailwing 4.1 siguiendo documentacion de web oficial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ function Home() {
     <div className="d-flex justify-content-center align-items-center vh-100 bg-white text-dark">
       <div className="text-center">
         <h1 className="display-4">Bienvenido a Vehículos Express</h1>
+        <h1 className="text-3xl font-bold text-blue-600 !text-blue-600">¡Tailwind funcionando!</h1>
         <p className="lead">Explora nuestra selección de autos nuevos y usados.</p>
       </div>
     </div>
