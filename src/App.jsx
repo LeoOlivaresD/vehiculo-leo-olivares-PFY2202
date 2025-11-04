a697450d4e4b96b25df1a960364785059ec70825
LeoOlivaresD <cspraword@gmail.com>
2025-11-03 23:23:03 -0300

    Se desarrolla componente para listar vehiculos y se implmenta useEffect aca para actulizar la informacion con useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import QuienesSomos from "./pages/QuienesSomos";
 import Contacto from "./pages/Contacto";
 import RegistrarVehiculo from "./pages/RegistroVehiculo";
-import ListaVehiculos from "./pages/ListarVehiculos";
+import ListarVehiculos from "./pages/ListarVehiculos";
 import MenuInferior from "./components/MenuInferior";
 
 function App() {
@@ -16,8 +16,19 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/quienes-somos" element={<QuienesSomos />} />
           <Route path="/contacto" element={<Contacto />} />
-          <Route path="/registroVehiculo" element={<RegistrarVehiculo />} />
-          <Route path="/listarVehiculos" element={<ListaVehiculos />} />
+          <Route
+            path="/registroVehiculo"
+            element={
+              <RegistrarVehiculo
+                vehiculos={vehiculos}
+                setVehiculos={setVehiculos}
+              />
+            }
+          />
+          <Route
+            path="/listarVehiculos"
+            element={<ListarVehiculos vehiculos={vehiculos} />}
+          />
         </Routes>
         <MenuInferior />
       </div>
--- src/pages/ListarVehiculos.jsx
+++ src/pages/ListarVehiculos.jsx
@@ -1,9 +1,40 @@
-function listarVehiculos() {
+import { useEffect } from "react";
+import Cabecera from "../components/Header";
+
+function ListarVehiculos({ vehiculos = [] }) {
+  useEffect(() => {
+    console.log("Vehículos actualizados:", vehiculos);
+  }, [vehiculos]);
+
   return (
     <div>
-      <h1>Listado de Vehículos</h1>
-      {/* Aquí iría la lógica para listar los vehículos */}
+      <Cabecera title="Lista de Vehículos" />
+      <div className="container mt-4">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Marca</th>
+              <th>Modelo</th>
+              <th>Precio</th>
+              <th>Año</th>
+              <th>Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vehiculos.map((v, i) => (
+              <tr key={i}>
+                <td>{v.marca}</td>
+                <td>{v.modelo}</td>
+                <td>{v.precio}</td>
+                <td>{v.año}</td>
+                <td>{v.descripcion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-export default listarVehiculos;
+
+export default ListarVehiculos;
